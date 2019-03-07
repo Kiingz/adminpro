f8e5eb3b1d9b64565080ab32ce775f36a11bb977
Alejandro Reyes <reiies094@gmail.com>
2019-03-06 23:56:15 -0300

    Rutas Hijas -ForChild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,6 @@ import { PagesModule } from './pages/pages.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-// import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-// import { DashboardComponent } from './pages/dashboard/dashboard.component';
-// import { ProgressComponent } from './pages/progress/progress.component';
-// import { Graficas1Component } from './pages/graficas1/graficas1.component';
-// import { PagesComponent } from './pages/pages.component';
-// import { HeaderComponent } from './shared/header/header.component';
-// import { SidebarComponent } from './shared/sidebar/sidebar.component';
-// import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-
 
 
 
@@ -26,15 +17,7 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
-    // NopagefoundComponent,
-    // DashboardComponent,
-    // ProgressComponent,
-    // Graficas1Component,
-    // PagesComponent,
-    // HeaderComponent,
-    // SidebarComponent,
-    // BreadcrumbsComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
