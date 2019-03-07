b3e6db4d1147ff61476df1505d571ec33135a8f3
Alejandro Reyes <reiies094@gmail.com>
2019-03-06 23:59:43 -0300

    Limpieza y Format-Prietter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { APP_ROUTES } from './app.routes';
+import { APP_ROUTES } from "./app.routes";
 
-import { PagesModule } from './pages/pages.module';
+import { PagesModule } from "./pages/pages.module";
 
-
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './login/register.component';
-
-
-
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./login/register.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    BrowserModule,
-    APP_ROUTES,
-    PagesModule
-  ],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, APP_ROUTES, PagesModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
