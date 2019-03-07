400869906bc754b89980cde49a28bf8c5327433d
Alejandro Reyes <reiies094@gmail.com>
2019-03-06 23:45:04 -0300

    Modulos Personalizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { APP_ROUTES } from './app.routes';
 
+import { PagesModule } from './pages/pages.module';
+
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+// import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+// import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { ProgressComponent } from './pages/progress/progress.component';
+// import { Graficas1Component } from './pages/graficas1/graficas1.component';
+// import { PagesComponent } from './pages/pages.component';
+// import { HeaderComponent } from './shared/header/header.component';
+// import { SidebarComponent } from './shared/sidebar/sidebar.component';
+// import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+
 
 
 
@@ -22,20 +26,20 @@ import { RegisterComponent } from './login/register.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent
-    
+    RegisterComponent,
+    // NopagefoundComponent,
+    // DashboardComponent,
+    // ProgressComponent,
+    // Graficas1Component,
+    // PagesComponent,
+    // HeaderComponent,
+    // SidebarComponent,
+    // BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
