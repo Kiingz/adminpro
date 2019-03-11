2a97ee97f8d22a539f34383510b6e205c5789f4a
Alejandro Reyes <reiies094@gmail.com>
2019-03-11 18:05:02 -0300

    Metodo Retry de un observable

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { retry } from "rxjs/operators";
 
 @Component({
-  selector: 'app-rxjs',
-  templateUrl: './rxjs.component.html',
+  selector: "app-rxjs",
+  templateUrl: "./rxjs.component.html",
   styles: []
 })
 export class RxjsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    let obs = new Observable(observer => {
+      let contador = 0;
+      let intervalo = setInterval(() => {
+        contador += 1;
+        observer.next(contador);
+        if (contador === 3) {
+          clearInterval(intervalo);
+          observer.complete();
+        }
+        if (contador === 2) {
+          //clearInterval(intervalo);
+          observer.error("Help!");
+        }
+      }, 1000);
+    });
+    obs
+      .pipe(retry(3))
+      .subscribe(
+        numero => console.log("Subs ", numero),
+        error => console.error("Error en el obs", error),
+        () => console.log("El observador Termino!")
+      );
   }
 
+  ngOnInit() {}
 }
