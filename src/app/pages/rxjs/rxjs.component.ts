ba79e0e7831d8e6cb1bdfa0b064c6d6fabcc7f9a
Alejandro Reyes <reiies094@gmail.com>
2019-03-11 18:13:07 -0300

    FUncion que retorna un observable

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -9,7 +9,19 @@ import { retry } from "rxjs/operators";
 })
 export class RxjsComponent implements OnInit {
   constructor() {
-    let obs = new Observable(observer => {
+    this.regresaObserbavle()
+      .pipe(retry(3))
+      .subscribe(
+        numero => console.log("Subs ", numero),
+        error => console.error("Error en el obs", error),
+        () => console.log("El observador Termino!")
+      );
+  }
+
+  ngOnInit() {}
+
+  regresaObserbavle(): Observable<number> {
+    return new Observable<number>(observer => {
       let contador = 0;
       let intervalo = setInterval(() => {
         contador += 1;
@@ -24,14 +36,5 @@ export class RxjsComponent implements OnInit {
         }
       }, 1000);
     });
-    obs
-      .pipe(retry(3))
-      .subscribe(
-        numero => console.log("Subs ", numero),
-        error => console.error("Error en el obs", error),
-        () => console.log("El observador Termino!")
-      );
   }
-
-  ngOnInit() {}
 }
