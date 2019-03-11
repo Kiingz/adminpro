c443e86d165de36d2e998397c7df4a81b466a09b
Alejandro Reyes <reiies094@gmail.com>
2019-03-11 18:52:42 -0300

    Breadcrums usando observables, parametros de rutas

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivationEnd } from "@angular/router";
+import { filter, map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-breadcrumbs',
-  templateUrl: './breadcrumbs.component.html',
+  selector: "app-breadcrumbs",
+  templateUrl: "./breadcrumbs.component.html",
   styles: []
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  titulo: string;
+  constructor(private router: Router) {
+    this.getDataRoute().subscribe(data => {
+      console.log(data);
+      this.titulo = data.titulo;
+    });
   }
 
+  ngOnInit() {}
+
+  getDataRoute() {
+    return this.router.events.pipe(
+      filter(evento => evento instanceof ActivationEnd),
+      filter((evento: ActivationEnd) => evento.snapshot.firstChild === null),
+      map((evento: ActivationEnd) => evento.snapshot.data)
+    );
+  }
 }
