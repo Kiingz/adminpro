3511b157f88d2944a11f706cbe7ada00f4c2d140
Alejandro Reyes <reiies094@gmail.com>
2019-03-25 21:50:24 -0300

    Correccion de error - Login token google

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,14 +5,14 @@ import { UsuarioService } from "../services/service.index";
 import { Usuario } from "../models/usuario.model";
 
 declare function init_plugins();
-declare const gapi:any;
+declare const gapi: any;
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  email:string;
+  email: string;
   recuerdame: boolean = false;
   auth2: any;
 
@@ -23,29 +23,32 @@ export class LoginComponent implements OnInit {
     this.googleInit();
     // el || hace que this.email tome el valor siguiente en caso
     // de que no exista el 'email' en el localStorage
-    this.email = localStorage.getItem('email') || '';
-    if( this.email.length > 0){
+    this.email = localStorage.getItem("email") || "";
+    if (this.email.length > 0) {
       this.recuerdame = true;
     }
   }
 
-  googleInit(){
-    gapi.load('auth2', ()=>{
+  googleInit() {
+    gapi.load("auth2", () => {
       this.auth2 = gapi.auth2.init({
-        client_id: '195195714072-bi1o5j0vmiit3es6h8aps5p4ekv2lf9h.apps.googleusercontent.com',
-        cookiepolicy: 'single_host_origin',
-        scope: 'profile email'
+        client_id:
+          "195195714072-bi1o5j0vmiit3es6h8aps5p4ekv2lf9h.apps.googleusercontent.com",
+        cookiepolicy: "single_host_origin",
+        scope: "profile email"
       });
-      this.attachSignin(document.getElementById('btnGoogle'));
-    })
-  };
+      this.attachSignin(document.getElementById("btnGoogle"));
+    });
+  }
 
-  attachSignin( element ){
-    this.auth2.attachClickHandler(element, {}, (googleUser) =>{
+  attachSignin(element) {
+    this.auth2.attachClickHandler(element, {}, googleUser => {
       // let token = googleUser.getBasicProfile();
       let token = googleUser.getAuthResponse().id_token;
-      this._usuarioService.loginGoogle( token ).subscribe( ()=> window.location.href = '#/dashboard');
-    })
+      this._usuarioService
+        .loginGoogle(token)
+        .subscribe(() => (window.location.href = "#/dashboard"));
+    });
   }
 
   ingresar(forma: NgForm) {
@@ -56,7 +59,7 @@ export class LoginComponent implements OnInit {
 
     this._usuarioService
       .login(usuario, forma.value.recuerdame)
-      .subscribe( correcto => this.router.navigate(['/dashboard']));
+      .subscribe(correcto => this.router.navigate(["/dashboard"]));
     // this.router.navigate(['/dashboard'])
   }
 }
