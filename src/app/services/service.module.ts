7346ad154989dc215bc1f5e0962caa7f066eda5f
Alejandro Reyes <reiies094@gmail.com>
2019-03-27 15:06:46 -0300

    Servicio de carga de archivos( imagenes )

--- src/app/services/service.module.ts
+++ src/app/services/service.module.ts
@@ -2,11 +2,18 @@ import { CommonModule } from '@angular/common';
 // tslint:disable-next-line: no-submodule-imports
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { LoginGuardGuard, SettigsService, SharedService, SidebarService, UsuarioService } from './service.index';
+import {
+	LoginGuardGuard,
+	SettigsService,
+	SharedService,
+	SidebarService,
+	SubirArchivoService,
+	UsuarioService
+} from './service.index';
 
 @NgModule({
 	declarations: [],
 	imports: [ CommonModule, HttpClientModule ],
-	providers: [ SettigsService, SidebarService, SharedService, UsuarioService, LoginGuardGuard ]
+	providers: [ SettigsService, SidebarService, SharedService, UsuarioService, LoginGuardGuard, SubirArchivoService ]
 })
 export class ServiceModule {}
