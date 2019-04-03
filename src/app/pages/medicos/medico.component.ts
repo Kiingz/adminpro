58f167d1ee534b4a587529a0576b9e2f2283256d
Alejandro Reyes <reiies094@gmail.com>
2019-04-03 16:00:16 -0300

    Mostrar Informacion de hospital seleccionado

--- src/app/pages/medicos/medico.component.ts
+++ src/app/pages/medicos/medico.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Hospital } from 'src/app/models/hospital.model';
 import { Medico } from 'src/app/models/medico.model';
 import { HospitalService, MedicoService } from 'src/app/services/service.index';
@@ -11,8 +12,14 @@ import { HospitalService, MedicoService } from 'src/app/services/service.index';
 })
 export class MedicoComponent implements OnInit {
 	hospitales: Hospital[] = [];
-	medico: Medico = new Medico();
-	constructor(public _medicoService: MedicoService, public _hospitalService: HospitalService) {}
+	medico: Medico = new Medico('', '', '', '', '');
+	hospital: Hospital = new Hospital('');
+
+	constructor(
+		public _medicoService: MedicoService,
+		public _hospitalService: HospitalService,
+		public router: Router
+	) {}
 
 	ngOnInit() {
 		this._hospitalService.cargarHospitales().subscribe((hospitales) => (this.hospitales = hospitales));
@@ -23,7 +30,12 @@ export class MedicoComponent implements OnInit {
 			return;
 		}
 		this._medicoService.guardarMedico(this.medico).subscribe((medico) => {
-			console.log(medico);
+			this.medico._id = medico._id;
+			this.router.navigate([ '/medico', medico._id ]);
 		});
+	}
+
+	cambioHospital(id: string) {
+		this._hospitalService.obtenerHospital(id).subscribe((hospital) => (this.hospital = hospital));
 	}
 }
